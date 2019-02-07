2e92d5b82ea2a4ac75325b477189211099f2e642
William Weiss <william@codification.org>
2019-02-07 10:33:20 -0500

    feat: add utility method for stream to buffer conversion

--- src/IOUtils.ts
+++ src/IOUtils.ts
@@ -1,3 +1,4 @@
+import { Observable, Observer } from 'rxjs';
 import { Readable } from 'stream';
 
 export class IOUtils {
@@ -11,5 +12,22 @@ export class IOUtils {
     rval.push(buffer);
     rval.push(null);
     return rval;
+  }
+
+  /**
+   * Returns a `Buffer`  containing the data from the specified `Readable`.
+   *
+   * @param stream - The `Readable` containing the data to be buffered.
+   */
+  public static readableToBuffer(stream: Readable): Observable<Buffer> {
+    const rval = [];
+    return Observable.create((observer: Observer<Buffer>) => {
+      stream.on('data', data => rval.push(data));
+      stream.on('error', err => observer.error(err));
+      stream.on('end', () => {
+        observer.next(Buffer.concat(rval));
+        observer.complete();
+      });
+    });
   }
 }
